757c0e6f256c19a2a0e55b9eca60999b91def13f
Calvin Jones <calvinj89@gmail.com>
2013-09-22 23:39:23 -0400

    09/22/2013 Changes

--- whozdahottest/src/main/java/com/whozdahottest/controller/LoginController.java
+++ whozdahottest/src/main/java/com/whozdahottest/controller/LoginController.java
@@ -8,20 +8,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController {
  
-	@RequestMapping(value="/hello", method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
- 
-		model.addAttribute("message", "Spring 3 MVC Hello World");
-		return "hello";
-		//return "redirect:/views/hello.html";
-	}
-	
-	
+		
 	@RequestMapping(value="/login", method = RequestMethod.GET)
 	public String login(ModelMap model) {
  
 		return "login";
+ 	}
+	
+	@RequestMapping(value="/welcome", method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
  
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		return "welcome";
+		//return "redirect:/views/hello.html";
 	}
  
 }
